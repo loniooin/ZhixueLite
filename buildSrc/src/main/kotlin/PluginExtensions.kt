bf7fb659518b40ca9856119d0074b54af1c1d35e
loniooin <loniooin@gmail.com>
2022-10-12 21:41:18 +0800

    Add Plugins

--- buildSrc/src/main/kotlin/PluginExtensions.kt
+++ buildSrc/src/main/kotlin/PluginExtensions.kt
@@ -1,8 +1,14 @@
 import org.gradle.plugin.use.PluginDependenciesSpec
 import org.gradle.plugin.use.PluginDependencySpec
 
+inline val PluginDependenciesSpec.library: PluginDependencySpec
+    get() = id("com.android.library")
+
 inline val PluginDependenciesSpec.application: PluginDependencySpec
     get() = id("com.android.application")
+
+inline val PluginDependenciesSpec.kotlinKapt: PluginDependencySpec
+    get() = id("org.jetbrains.kotlin.kapt")
 
 inline val PluginDependenciesSpec.kotlinAndroid: PluginDependencySpec
     get() = id("org.jetbrains.kotlin.android")
