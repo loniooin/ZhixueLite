13ab4518bb53a3eb09f13761e106354264db1c3a
loniooin <loniooin@gmail.com>
2022-10-12 21:19:20 +0800

    Add Hilt And Network Libraries

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -8,4 +8,18 @@ object Libs {
         const val kotlinGradle = "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinGradleVersion"
         const val androidGradle = "com.android.tools.build:gradle:$androidGradleVersion"
     }
+    
+    object Hilt {
+        private const val hiltVersion = "2.44"
+        
+        const val hiltAndroid = "com.google.dagger:hilt-android:$hiltVersion"
+        const val hiltCompiler = "com.google.dagger:hilt-compiler:$hiltVersion"
+    }
+    
+    object Retrofit {
+        private const val retrofitVersion = "2.9.0"
+        
+        const val retrofit2 = "com.squareup.retrofit2:retrofit:$retrofitVersion"
+        const val retrofitConverter = "com.squareup.retrofit2:converter-moshi:$retrofitVersion"
+    }
 }
