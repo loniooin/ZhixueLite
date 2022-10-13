609182cd5e9e53d83c1e878bd5cd5357c5e49107
loniooin <loniooin@gmail.com>
2022-10-13 13:03:46 +0800

    Add Compose Libraries

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -16,6 +16,16 @@ object Libs {
         const val hiltCompiler = "com.google.dagger:hilt-compiler:$hiltVersion"
     }
     
+    object Compose {
+        private const val composeVersion = "1.2.1"
+        private const val composeActivityVersion = "1.6.0"
+        
+        const val activity = "androidx.activity:activity-compose:$composeActivityVersion"
+        const val material = "androidx.compose.material:material:$composeVersion"
+        const val uiTooling = "androidx.compose.ui:ui-tooling:$composeVersion"
+        const val animation = "androidx.compose.animation:animation:$composeVersion"
+    }
+    
     object Retrofit {
         private const val retrofitVersion = "2.9.0"
         
