2d52f01d7c8ebcfbdeb82c7e3d5fdfa2b4543e0c
loniooin <loniooin@gmail.com>
2022-10-14 16:10:20 +0800

    Configure App Module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     application
+    kotlinKapt
     kotlinAndroid
+    hiltAndroid
 }
 
 android {
@@ -14,4 +16,33 @@ android {
         versionCode = Configurations.versionCode
         versionName = Configurations.versionName
     }
+    
+    buildTypes {
+        getByName(Configurations.release) {
+            isMinifyEnabled = Configurations.minifyEnabled
+            isShrinkResources = Configurations.shrinkResourcesEnabled
+            proguardFile(getDefaultProguardFile(Configurations.defProguardFile))
+        }
+    }
+    
+    buildFeatures {
+        compose = Configurations.composeEnabled
+    }
+    
+    composeOptions {
+        kotlinCompilerExtensionVersion = Configurations.composeCompilerVersion
+    }
+}
+
+dependencies {
+    implementation(Libs.Hilt.hiltAndroid)
+    implementation(Libs.Hilt.hiltNavigation)
+    implementation(Libs.Compose.activity)
+    implementation(Libs.Compose.material)
+    implementation(Libs.Compose.uiTooling)
+    implementation(Libs.Compose.animation)
+    implementation(Libs.Compose.navigation)
+    implementation(Libs.AndroidX.splashScreen)
+    implementation(project(Libs.Module.coreDesign))
+    kapt(Libs.Hilt.hiltCompiler)
 }
